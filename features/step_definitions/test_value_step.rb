1e65e810c4699060376c4a6092898c36f6a3c871
howardreith <howard.reith@gmail.com>
2018-10-14 07:21:49 -0400

    Add functioning Watir
    -The test can successfully see that txt_val_1 exists in the browser.

---
+++ features/step_definitions/test_value_step.rb
@@ -0,0 +1,8 @@
+require 'watir'
+require 'rubygems'
+require 'selenium-webdriver'
+
+browser = Watir::Browser.start 'http://localhost:7165/'
+text1 = browser.span id: 'txt_val_1'
+puts 'text1 exists?'
+puts text1.exists?
