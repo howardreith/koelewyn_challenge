94368c92538d8651b62a47e0f4713e5d324831cb
howardreith <howard.reith@gmail.com>
2018-10-15 09:06:19 -0400

    Total tester now functioning
    -Total tester now properly testing for total.
    -Note that the figures on the page do not add to the total listed.

--- features/step_definitions/page_tester_steps.rb
+++ features/step_definitions/page_tester_steps.rb
@@ -26,17 +26,63 @@ Given(/^I have navigated to the web page$/) do
 end
 
 When(/^the span's value is greater than 0$/) do
-  element1 = @browser.span id: 'txt_val_1'
-  puts(element1)
-  value1 = element1.text.to_s.tr('$,', '')
-  value1 = value1.to_f
-  puts(value1)
-  if value1 > 0
-    @answer = true
+  texts = []
+  texts[0] = @browser.span id: 'txt_val_1'
+  texts[1] = @browser.span id: 'txt_val_2'
+  texts[2] = @browser.span id: 'txt_val_4'
+  texts[3] = @browser.span id: 'txt_val_5'
+  texts[4] = @browser.span id: 'txt_val_6'
+
+  values = []
+  values[0] = texts[0].text.to_s.tr('$,', '').to_f
+  values[1] = texts[1].text.to_s.tr('$,', '').to_f
+  values[2] = texts[2].text.to_s.tr('$,', '').to_f
+  values[3] = texts[3].text.to_s.tr('$,', '').to_f
+  values[4] = texts[4].text.to_s.tr('$,', '').to_f
+
+  values.each do |value|
+    if value > 0
+      @value_answer = true
+    else
+      @value_answer = false
+    end
   end
-  puts @answer
 end
 
 Then(/^I should get true$/) do
-  expect(@answer).to eq(true)
+  expect(@value_answer).to eq(true)
+end
+
+Given(/^The user has navigated to the web page$/) do
+  @browser.goto('http://localhost:7165/')
+end
+
+When(/^The total value is equal to the sum of the span values$/) do
+  texts = []
+  texts[0] = @browser.span id: 'txt_val_1'
+  texts[1] = @browser.span id: 'txt_val_2'
+  texts[2] = @browser.span id: 'txt_val_4'
+  texts[3] = @browser.span id: 'txt_val_5'
+  texts[4] = @browser.span id: 'txt_val_6'
+
+  values = []
+  values[0] = texts[0].text.to_s.tr('$,', '').to_f
+  values[1] = texts[1].text.to_s.tr('$,', '').to_f
+  values[2] = texts[2].text.to_s.tr('$,', '').to_f
+  values[3] = texts[3].text.to_s.tr('$,', '').to_f
+  values[4] = texts[4].text.to_s.tr('$,', '').to_f
+  @sum = values.inject(0){|sum,value| sum + value }
+
+  total_text = @browser.span id: 'txt_ttl_val'
+  total = total_text.text.to_s.tr('$,', '').to_f
+
+  if @sum == total
+    @total_answer = true
+  else
+    @total_answer = false
+  end
+end
+
+Then(/^I should see a return of true$/) do
+  expect(@total_answer).to eq(true)
 end
